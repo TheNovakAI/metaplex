fdc715071629922b4197f00966baf89a95d35ea4
ryguy077 <ryannovak07@gmail.com>
2022-01-22 15:58:59 -0600

    uu mint 2

--- js/packages/candy-machine-ui/src/Home.tsx
+++ js/packages/candy-machine-ui/src/Home.tsx
@@ -163,7 +163,7 @@ const Home = (props: HomeProps) => {
 
   return (
     <Container style={{ marginTop: 0 }}>
-    <img src="SSMint.png" alt="SolSand" className="logo" />
+    <img src="SS-MINT2.png" alt="SolSand" className="logo" />
     <br></br>
     <br></br>
     <br></br>
